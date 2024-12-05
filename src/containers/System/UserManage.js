c8325adc0162eeda58f9424615ca74035845023e
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2024-12-05 21:08:16 +0700

    feat: get listusers by using api

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,75 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import { getAllUsers } from "../../services/userService";
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            listUsers: [],
+        };
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        const response = await getAllUsers("ALL");
+        console.log(response);
+        if (response && response.errCode === 0) {
+            this.setState({
+                listUsers: response.users,
+            });
+        }
     }
-
 
     render() {
+        const listUsers = this.state.listUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container mt-5">
+                <div className="text-center user-management-title">Manage users ThanhNhangg</div>
+                <table className="table mt-5">
+                    <thead className="table-dark">
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {listUsers &&
+                            listUsers.length > 0 &&
+                            listUsers.map((item, index) => {
+                                return (
+                                    <tr key={item.id}>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.lastName}</td>
+                                        <td>{item.address}</td>
+                                        <td className="d-flex">
+                                            <button className="btn btn-warning me-3">
+                                                <i className="fas fa-pencil-alt"></i>
+                                            </button>
+                                            <button className="btn btn-danger ">
+                                                <i className="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                    </tbody>
+                </table>
+            </div>
         );
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,6 +3,11 @@ const handleLogin = (email, password) => {
     return axios.post("/api/login", {email, password});
 };
 
+const getAllUsers = (id) => {
+    return axios.get(`/api/get-all-user?id=${id}`);
+}
+
 export {
     handleLogin,
+    getAllUsers,
 }
