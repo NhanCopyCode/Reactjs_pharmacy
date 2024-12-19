7b48a933570c006f8e5164c92ad7fc7b9d6b12f6
thanhnhanggg <lephuocthanhnhat0409@gmail.com>
2024-12-19 11:57:22 +0700

    style: css section react slick

--- src/containers/HomePage/Section/Sepecialty.js
+++ src/containers/HomePage/Section/Sepecialty.js
@@ -9,13 +9,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import NextArrow from "./NextArrow";
 import PrevArrow from "./PrevArrow";
-
+import sepcialty_image from "../../../assets/images/specialty/specialty_image.jpg";
 class Sepecialty extends Component {
     render() {
         let settings = {
             dots: false,
-            infinite: true,
             speed: 300,
+            infinite: true,
             slidesToShow: 4,
             slidesToScroll: 1,
             nextArrow: <NextArrow />,
@@ -24,18 +24,65 @@ class Sepecialty extends Component {
         return (
             <section className="section_sepecialty">
                 <div className="container">
+                    <div className="specialty_header">
+                        <h1>Chuyên khoa phổ biến</h1>
+                        <button className="see_more">Xem thêm</button>
+                    </div>
                     <Slider {...settings} className="slider">
-                        <a href="#" className="img-customize">
-                            Image 1
+                        <a href="#" className="slide_item_link">
+                            <div className="slider_item_content">
+                                <img
+                                    src={sepcialty_image}
+                                    alt="Specialty image"
+                                />
+                                <div className="slide_item_title">
+                                    <span>Cơ xương khớp</span>
+                                </div>
+                            </div>
                         </a>
-                        <a href="#" className="img-customize">
-                            Image
+                        <a href="#" className="slide_item_link">
+                            <div className="slider_item_content">
+                                <img
+                                    src={sepcialty_image}
+                                    alt="Specialty image"
+                                />
+                                <div className="slide_item_title">
+                                    <span>Cơ xương khớp</span>
+                                </div>
+                            </div>
                         </a>
-                        <a href="#" className="img-customize">
-                            Image
+                        <a href="#" className="slide_item_link">
+                            <div className="slider_item_content">
+                                <img
+                                    src={sepcialty_image}
+                                    alt="Specialty image"
+                                />
+                                <div className="slide_item_title">
+                                    <span>Cơ xương khớp</span>
+                                </div>
+                            </div>
                         </a>
-                        <a href="#" className="img-customize">
-                            Image
+                        <a href="#" className="slide_item_link">
+                            <div className="slider_item_content">
+                                <img
+                                    src={sepcialty_image}
+                                    alt="Specialty image"
+                                />
+                                <div className="slide_item_title">
+                                    <span>Cơ xương khớp</span>
+                                </div>
+                            </div>
+                        </a>
+                        <a href="#" className="slide_item_link">
+                            <div className="slider_item_content">
+                                <img
+                                    src={sepcialty_image}
+                                    alt="Specialty image"
+                                />
+                                <div className="slide_item_title">
+                                    <span>Cơ xương khớp</span>
+                                </div>
+                            </div>
                         </a>
                     </Slider>
                 </div>
